59cacdd1d85360d84febe9b4a94a2ce37cf832a6
Nicholas Rennie <nrennie3@gmail.com>
2026-02-28 00:28:03 -0800

    Added no search engine crawling

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -29,6 +29,10 @@ export const metadata: Metadata = {
     locale: "en_US",
     type: "website",
   },
+  robots: {
+    index: false,
+    follow: false,
+  },
 };
 
 export default function RootLayout({
